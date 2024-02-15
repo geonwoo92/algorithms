a230d768d410fa02bfd4856fd5b31385d65ef1f8
geonwoo92 <cnu201102054@gamil.com>
2024-02-15 17:40:39 +0900

    20240215 for-loop

---
+++ Arraytext.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Arraytext {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = 3;
+        int [] arr = {2,4,6,8,10};
+
+        System.out.println("arr 의 길이:"+arr.length);
+        for(int i=0;i<arr.length;i++){
+
+
+            System.out.println(arr[i]);
+        }
+
+
+    }
+}

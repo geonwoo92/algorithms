73bc3801f6f700a70bd3d0cdbc4956f2c149e1b0
geonwoo92 <cnu20110205@gamil.com>
2024-02-22 14:12:25 +0900

    12313

--- Gugudan.java
+++ Gugudan.java
@@ -3,19 +3,23 @@ import java.util.Scanner;
 public class Gugudan {
     public static void main(String[] args) {
 
-        int n = 1;
 
         for (int i = 1; i < 10; i++) {
-            for (int j = 1; j < i; j++) {
+            for (int j = 2; j < 6; j++) {
 
-                System.out.println( j + " * " + i + " = " + j * i);
-
-
+                System.out.printf(" %3d * %d = %3d ", j, i, j * i);
             }
+            System.out.printf("%n");
+        }
+        System.out.printf("%n");
 
+        for (int i = 1; i < 10; i++) {
 
-            n++;
+            for (int j = 6; j < 10; j++) {
 
+                System.out.printf(" %3d * %d = %3d ", j, i, j * i);
+            }
+            System.out.printf("%n");
         }
     }
 }

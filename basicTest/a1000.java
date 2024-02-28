0887f2a22f08228e88e7a12519ea6a2451b5309c
geonwoo92 <cnu201102054@gmail.com>
2024-02-28 20:09:24 +0900

    240228

---
+++ basicTest/a1000.java
@@ -0,0 +1,13 @@
+package basicTest;
+
+import java.util.Scanner;
+
+public class a1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+
+
+    }}

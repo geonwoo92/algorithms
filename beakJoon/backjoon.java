2e995af94d5f7fff133f48d9f3245d172d4125d4
geonwoo92 <cnu201102054@gmail.com>
2024-03-18 18:40:57 +0900

    240318

---
+++ beakJoon/backjoon.java
@@ -0,0 +1,25 @@
+package beakJoon;
+
+import java.util.Scanner;
+
+public class backjoon {
+
+    public class Main {
+        public static void main(String args[]){
+
+            Scanner in=new Scanner(System.in);
+
+            while(true){
+
+                int A=in.nextInt();
+                int B=in.nextInt();
+
+                if(A==0 && B==0){
+                    in.close();
+                    break;
+                }
+                System.out.println(A+B);
+            }
+        }
+    }
+}
